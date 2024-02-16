e8e6731dd8223183cfc82bcc0253cfb6594cb1ad
Arthur Lecomte <arthur.lecomte@etu.univ-amu.fr>
2024-02-16 11:46:54 +0100

    ouais

--- src/Controller/DiscothequeController.php
+++ src/Controller/DiscothequeController.php
@@ -4,5 +4,9 @@ namespace App\Controller;
 
 class DiscothequeController
 {
-
+    public function triParFruit(&$array){
+        usort($array, function($a, $b){
+            //return strcmp($a->)
+        });
+    }
 }
